435a9b2a765fd81c7d4a0781a062b492c16caf2f
responsive-we <98676194+responsive-we@users.noreply.github.com>
2024-07-09 23:17:37 +0530

    commit

--- src/components/ReferralForm.jsx
+++ src/components/ReferralForm.jsx
@@ -6,7 +6,7 @@ const ReferralForm = ({ onClose, onSuccess }) => {
 
   const onSubmit = async (data) => {
     try {
-      const response = await fetch('https://accredian-backend-task-yme9.onrender.com:3001/api/referrals', {
+      const response = await fetch('https://accredian-backend-task-yme9.onrender.com:10000/api/referrals', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
